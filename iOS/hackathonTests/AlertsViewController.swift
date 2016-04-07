5de7f3669dda383d162707c54cb471ec66d7fe7a
yann breleur <yann.breleur@gmail.com>
2016-04-07 14:38:02 +0200

    nettoyage du code

--- iOS/hackathonTests/AlertsViewController.swift
+++ iOS/hackathonTests/AlertsViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class AlertsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AlertsViewController: UIViewController {
+    @IBOutlet weak var ViewQuestion: UIView!
+    @IBOutlet weak var btnEnRisque: UIButton!
+    @IBOutlet weak var titreQuestion: UILabel!
+    @IBOutlet weak var btnEnSurete: UIButton!
+    @IBOutlet weak var messageOk: UILabel!
+    
+    
+    @IBOutlet weak var viewMessage: UIView!
+    @IBOutlet weak var titreMessage: UILabel!
+    @IBOutlet weak var descriptionMEssage: UILabel!
+    
     @IBOutlet
     var alertsTableView: UITableView!
     var items: [String] = ["Bonjour, un incident majeur a eu lieu au sein de votre périmètre d’habitation. Merci de nous confirmer que vous êtes en sécurité.", "Le 'niveau alerte attentat' est maintenu en Ile-de-France et la vigilance renforcée continue de s'appliquer sur le reste du territoire.  Le ministère demande à chacune et chacun de les respecter afin d'améliorer le niveau de sécurité "]
@@ -16,24 +27,8 @@ class AlertsViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.messageOk.text = self.items[0]
+        self.descriptionMEssage.text = self.items[1]
     }
-    
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count;
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return basicCellAtIndexPath(indexPath)
-    }
-    
-    func basicCellAtIndexPath(indexPath:NSIndexPath) -> AlertControllerCell {
-        let cell = self.alertsTableView.dequeueReusableCellWithIdentifier(identifientCell)! as! AlertControllerCell
-        cell.configure(self.items[indexPath.row], index: indexPath.row)
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("You selected cell #\(indexPath.row)!")
-    }
+
 }
